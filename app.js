c32871e02099c27c8286ab4ed38f7d20606687a4
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-04-26 18:27:07 +0530

    auth dashboard fix

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ app.get("/",(req , res) => {
     res.render('login');
 });
 
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
 // app.get('/query',(req , res) => {
     
 //     const {userId , status} = req.query;
